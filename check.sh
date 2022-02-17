118f0da5f7623b1c0a8d2008505081effe011c56
Jasper Edbrooke <jasperedbrooke@gmail.com>
2022-02-17 00:45:36 -0800

    add basic check

---
+++ check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# numbers from http://www.numberworld.org/digits/Pi/
+echo "checking 0-50"
+cmp <(echo "243f6a8885a308d313198a2e03707344a4093822299f31d008") <(./pi_spigot 0) && echo "success"
+echo "checking 50-100"
+cmp <(echo "2efa98ec4e6c89452821e638d01377be5466cf34e90c6cc0ac") <(./pi_spigot 50) && echo "success"
+echo "checking 9950-10000"
+cmp <(echo "f169b5f18a8c73ee0b5e57368f6c79f4bb7a595926aab49ec6") <(./pi_spigot 9950) && echo "success"
+echo "checking 99950-100000"
+cmp <(echo "443388751069558b3e62e612bc302ec487aa9a6ea22673c1a5") <(./pi_spigot 99950) && echo "success"
+echo "checking 999950-1000000"
+cmp <(echo "bc89273abbced2884adaa7f46c59b44c28e672c29ffd342362") <(./pi_spigot 999950) && echo "success"
--- makefile
+++ makefile
@@ -1,6 +1,14 @@
 CC=gcc
 CFLAGS=-O3 -lm 
 
-default: pi_spigot.c
+pi_spigot: pi_spigot.c
 	$(CC) -o pi_spigot pi_spigot.c $(CFLAGS)
 
+
+check: pi_spigot check.sh
+	./check.sh
+	
+
+.PHONY: clean
+clean:
+	rm -rf pi_spigot
